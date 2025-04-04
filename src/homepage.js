87d6f36bfadff87ff7c10e40013f249f15d6f417
Chideraeustace <122830692+Chideraeustace@users.noreply.github.com>
2025-04-04 10:43:32 +0000

    Update homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Events</a></li>
-                <li><a href="#">About Us</a></li>
+                <li><a href="#">About</a></li>
                 
             </ul>
             </div>
